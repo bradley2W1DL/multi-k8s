6f42c695ebddcda35d0b457bef1e95eb37329b1e
Bradley Berger <bradley.s.berger@gmail.com>
2020-09-04 11:51:15 -0600

    fix h1 change bug

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h3>Fib Calculator, with change!</h1>
+          <h3>Fib Calculator, with change!</h3>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
           <br/>
